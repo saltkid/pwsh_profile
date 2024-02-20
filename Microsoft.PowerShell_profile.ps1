a310a4fe910c77d1ad49611c4e72897e98d37f31
saltkid <ojisaltkid@gmail.com>
2024-02-21 01:06:39 +0800

    add oh-my-posh, plus bubblesextra theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,6 @@ Function ffs
     $SearchQuery = $args -join ' '
     Start-Process "firefox" "--search `"$SearchQuery`""
 }
+
+$themeName = "bubblesextra"
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$themeName.omp.json" | Invoke-Expression
