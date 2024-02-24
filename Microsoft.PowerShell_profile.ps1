1be3e538fd95afccfae6e4058ce225ebd91c5091
saltkid <ojisaltkid@gmail.com>
2024-02-24 15:22:22 +0800

    add quick searching and cd'ing to git repos

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,3 +8,23 @@ Function ffs
 
 $themeName = "bubblesextra"
 oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$themeName.omp.json" | Invoke-Expression
+
+function Invoke-SearchGitRepos
+{
+    $searchPaths = @(
+        "$env:userprofile/projects",
+        "$env:userprofile/work",
+        "$env:localappdata/nvim",
+        "$env:userprofile/documents/powershell",
+        "$env:userprofile/music")
+
+    $selected = $(fd -u -i -t d -F '.git' $searchPaths | sed 's/\\.git//' | fzf)
+    if ($selected)
+    {
+        Set-Location $selected
+    }
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+}
+Set-PSReadLineKeyHandler -Key "Ctrl+d" -ScriptBlock { Invoke-SearchGitRepos }
+
